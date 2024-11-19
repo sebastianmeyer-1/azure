2edb13a42101a06d4dcc011ac6b5d88c088d3feb
Sebastian Meyer <sebastian.meyer1@gmail.com>
2024-11-19 17:39:36 +0100

    Initial checkin

---
+++ KeyVault/Program.cs
@@ -0,0 +1,12 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+
+string tenantId = "<TENANT ID>";
+string clientId = "<CLIENT ID>";
+string clientSecret = "<CLIENT SECRET>";
+Uri vaultUri = new Uri("https://<key vault name>.vault.azure.net/");
+var credentials = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+var secretClient = new SecretClient(vaultUri, credentials);
+var secret = secretClient.GetSecret("<SECRET NAME>");
+Console.WriteLine(secret.Value.Value);
